92ef3c7e6af4da40113a0655d8b37d897e9f56c4
AbhishekWagh411 <abhishekwagh412@gmail.com>
2023-12-15 17:13:42 +0530

    SpringBoot Project

---
+++ Jakarta_EE/Spring_First_Demo-2/src/main/java/com/example/repositories/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional
+public interface StudentRepository extends JpaRepository<com.example.entity.Student,Integer> 
+{
+}
